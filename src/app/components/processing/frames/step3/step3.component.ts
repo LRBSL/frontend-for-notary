7823db2320b8d043877fb663ba4a9f6fe5153049
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-15 16:19:22 +0530

    designed the header and nav-bar components

--- src/app/components/processing/frames/step3/step3.component.ts
+++ src/app/components/processing/frames/step3/step3.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-step3',
+  selector: 'app-processing-step3',
   templateUrl: './step3.component.html',
   styleUrls: ['./step3.component.css']
 })
--- src/app/components/processing/window/window.component.ts
+++ src/app/components/processing/window/window.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-window',
+  selector: 'app-processing-window',
   templateUrl: './window.component.html',
   styleUrls: ['./window.component.css']
 })
