37d89d60c0c66a873910843d6eafc33574f0c46f
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-15 13:57:52 +0530

    initialized components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { WindowComponent } from './components/processing/window/window.component';
+import { Step1Component } from './components/processing/frames/step1/step1.component';
+import { Step2Component } from './components/processing/frames/step2/step2.component';
+import { Step3Component } from './components/processing/frames/step3/step3.component';
+import { Step4Component } from './components/processing/frames/step4/step4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    WindowComponent,
+    Step1Component,
+    Step2Component,
+    Step3Component,
+    Step4Component
   ],
   imports: [
     BrowserModule
